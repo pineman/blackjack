a4801d0f2c84aafbfdeb348e63bd5ee0b050464c
pineman <joao.castropinheiro@gmail.com>
2016-05-01 19:31:13 +0100

    AMAZING

--- tmp/lists.c
+++ tmp/lists.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct list {
 	void *payload;
@@ -7,8 +8,22 @@ typedef struct list {
 	struct list *prev;
 } list;
 
-void 
+list *list_prepend(list **old_head, void *payload, size_t size)
+{
+	list *new_head = (list *) calloc((size_t) 1, sizeof(list));
+
+	new_head->payload = calloc((size_t) 1, size);
+	memcpy(new_head->payload, payload, size);
+	*old_head = new_head;
+
+	new_head->next = *old_head;
+}
+
 int main()
 {
+	list *head = NULL;
+	int inteiro = 5;
+	list_prepend(&head, &inteiro, sizeof(inteiro));
 
+	printf("%d\n", *( (int *) head->payload ));
 }
