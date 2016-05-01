71e8c046101019dca09bc9d982791df71d0f26b7
pineman <joao.castropinheiro@gmail.com>
2016-05-01 23:13:57 +0100

    lists are working again! everyone, get in here!

--- tmp/lists.c
+++ tmp/lists.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 // estes não ficam aqui!!
 #define	MAX_DECK_SIZE	52
 #define MAX_CARD_ID		13
@@ -26,6 +25,34 @@ typedef struct player {
 	char name[8];
 } player;
 
+void create_megadeck(list *head,int num_decks);
+void list_append(list *head,void *payload,size_t size);
+void list_prepend(list *head,void *payload,size_t size);
+void alloc_payload(list **node,void *payload,size_t size);
+
+int main()
+{
+	list dummy = {NULL, NULL, NULL};
+	list *head = &(dummy);
+
+	card cur_card = {clubs, 3};
+	list_append(head, &(cur_card), sizeof(card));
+	cur_card.id = 4;
+	list_append(head, &(cur_card), sizeof(card));
+	cur_card.id = 5;
+	list_prepend(head, &(cur_card), sizeof(card));
+	cur_card.id = 6;
+	list_append(head, &(cur_card), sizeof(card));
+	cur_card.id = 8;
+	list_append(head, &(cur_card), sizeof(card));
+
+	list *aux = head;
+	while (aux->next) {
+		printf("%d\n", ((card *) aux->next->payload)->id);
+		aux = aux->next;
+	}
+}
+
 void alloc_payload(list **node, void *payload, size_t size)
 {
 	(*node)->payload = calloc((size_t) 1, size);
@@ -34,49 +61,47 @@ void alloc_payload(list **node, void *payload, size_t size)
 
 void list_prepend(list *head, void *payload, size_t size)
 {
-	list *new_node = (list *) calloc((size_t) 1, sizeof(list));
+	list *new_head = (list *) calloc((size_t) 1, sizeof(list));
 
-	alloc_payload(&new_node, payload, size);
-	head->next = new_node;
+	alloc_payload(&new_head, payload, size);
+	new_head->next = head->next;
+	new_head->prev = head;
+	head->next = new_head;
 }
 
 void list_append(list *head, void *payload, size_t size)
 {
-	while(head->next)
-		head = head->next;
+	list *aux = head;
+	while(aux->next)
+		aux = aux->next;
+	list *tail = aux;
 
-	list *new_node = (list *) calloc((size_t) 1, sizeof(list));
+	list *new_tail = (list *) calloc((size_t) 1, sizeof(list));
 
-	alloc_payload(&new_node, payload, size);
-
-	head->next = new_node;
+	alloc_payload(&new_tail, payload, size);
+	new_tail->next = NULL;
+	new_tail->prev = tail;
+	tail->next = new_tail;
 }
 
 void create_megadeck(list *head, int num_decks)
 {
 	for(int i=1; i<=num_decks; i++) {
 		printf("num_decks = %d\n", i);
+
 		for(int j=clubs; j<spades; j++) {
 			printf("suit = %d\n", j);
+
 			for(int k=1; k < MAX_CARD_ID; k++) {
 				printf("card_id = %d\n", k);
 				card *cur_card = (card *) calloc(1, sizeof(card));
+
 				cur_card->id = k;
 				cur_card->suit = j;
+
 				printf("%d: %d", cur_card->suit, cur_card->id);
 				list_append(head, cur_card, sizeof(card));
 			}
 		}
 	}
-}
-
-
-int main()
-{
-	list dummy;
-	list *head = &(dummy);
-
-	card cur_card = {clubs, 3};
-	list_prepend(head, &(cur_card), sizeof(card));
-	printf("%d\n", ((card *) head->payload)->id);
 }
